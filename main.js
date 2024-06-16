9e7d39c6fbf762e99f18c9a313fe195832bc4061
soiderino <soider@protonmail.com>
2024-06-17 01:26:48 +0300

    🚀 code rework

--- main.js
+++ main.js
@@ -1,36 +1,28 @@
 window.addEventListener('load', () => {
   const handleMutation = (mutationsList) => {
-    for (let mutation of mutationsList) {
-      if (
-        mutation.type === 'attributes' &&
-        mutation.attributeName === 'class'
-      ) {
-        const target = mutation.target
-        if (
+    mutationsList.forEach((mut) => {
+      if (mut.type === 'attributes' && mut.attributeName === 'class') {
+        const target = mut.target
+        const emulateClick =
           target.title === 'Нажмите, если лень смотреть опенинг' ||
           (target.title === 'Перейти к следующему эпизоду' &&
             !target.classList.contains('vjs-hidden'))
-        ) {
-          console.log(
-            `%cJut.su Extended%c\n\n    github: https://github.com/soiderino/jut.su-extended\n    created by: soiderino 💜`,
-            `color: #293e29; padding: .2rem; border-radius: .3rem; background-color: #62955b; font-family: verdana; font-weight: bold; font-size: 9px; text-transform: uppercase;`,
-            `color: #ffffff`
-          )
+        if (emulateClick) {
           target.dispatchEvent(new Event('click'))
         }
       }
-    }
+    })
   }
 
   const observer = new MutationObserver(handleMutation)
 
   const intervalId = setInterval(() => {
     const skipOpening = document.querySelector('.vjs-overlay-bottom-left')
-    const skipEnding = document.querySelector('.vjs-overlay-bottom-right')
+    const nextEpisode = document.querySelector('.vjs-overlay-bottom-right')
 
     if (skipOpening) observer.observe(skipOpening, { attributes: true })
-    if (skipEnding) observer.observe(skipEnding, { attributes: true })
+    if (nextEpisode) observer.observe(nextEpisode, { attributes: true })
 
-    if (skipOpening || skipEnding) clearInterval(intervalId)
+    if (skipOpening || nextEpisode) clearInterval(intervalId)
   })
 })
